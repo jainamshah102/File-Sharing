696d52081618807c9630f9c8f0c575f7de9bf914
Jainam Shah <61734413+jainam2385@users.noreply.github.com>
2021-06-22 16:54:40 +0530

    Mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import Express from "express";
-
+import MongoDB from "./services/MongoDB";
 
 const app = Express();
 const PORT: string | number = process.env.PORT || 3000;
 
+MongoDB();
 
 app.listen(PORT, () => {
-    console.log(`Serving@ ${PORT}`)
+    console.log(`Serving @${PORT}`)
 });
---
+++ src/services/MongoDB.ts
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + '../../.env' });
+
+import mongoose from "mongoose";
+
+
+export default () => {
+
+    mongoose.connect(process.env.DATABASE_CONNECTION as string, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true
+    });
+
+    const connnection = mongoose.connection;
+
+    connnection.once('open', () => {
+        console.log('Database connected');
+    })
+};
